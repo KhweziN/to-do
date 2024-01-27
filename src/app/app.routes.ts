83e3f3a52278dce6369acd0e8646b1892b262455
Khwezi N <khwezisandi@gmail.com>
2024-01-27 12:51:56 +0200

    created a new to-do component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ToDoComponent } from './to-do/to-do.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ToDoComponent}
+];
